e7ece255bddd49c11d4c21feb01afeded04664dc
seshu <mulagani.seshu@gmail.com>
2022-04-12 19:41:56 +0530

    first commit

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from app.models import Topic
+from app.models import Access_Record, Topic, Webpage
 
 # Register your models here.
 admin.site.register(Topic)
+
+admin.site.register(Webpage)
+
+admin.site.register(Access_Record)
+
+
